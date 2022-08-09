c89e8f2f668006437019ea4e022a1e1173110518
tugsem <saranlitugsem@gmail.com>
2022-08-09 15:01:48 +0300

    Add book class

---
+++ book.rb
@@ -0,0 +1,25 @@
+require_relative 'item'
+
+class Book < Item
+  attr_accessor = :publisher, :cover_state
+
+  def initialize(publisher, cover_state)
+    super(genre, author, label, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+    @archived = archived
+  end
+
+
+  def can_be_archived?
+    if @archived || @cover_state == 'bad'
+      return true
+    else
+      return false
+    end
+  end
+
+end
+
+# book = Book.new('n', 'good')
+# puts book.can_be_archived?
