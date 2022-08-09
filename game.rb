4561f99d672b935c0aa1e4ce6ffc2df2285640a3
Ghilain <ishimweghilain5050@gmail.com>
2022-08-10 00:34:27 +0200

    Add test on Game class

--- game.rb
+++ game.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, last_played_at, archived: true)
+  def initialize(multiplayer, last_played_at)
     super(genre, author, label, publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
@@ -15,5 +15,5 @@ class Game < Item
   end
 end
 
-# game = Game.new('Ishi', 2)
+# game = Game.new('Ishi', 3)
 # puts game.can_be_archived?
