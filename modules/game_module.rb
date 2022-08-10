0ac7ca8cb7ce3e1f8b5d0cd0d20f1f2243c62e63
Ghilain <ishimweghilain5050@gmail.com>
2022-08-11 01:51:30 +0200

    Add module for creating new game

---
+++ modules/game_module.rb
@@ -0,0 +1,44 @@
+require_relative '../game'
+require_relative '../author'
+
+module CreateGames
+  def add_game
+    puts 'Name of Game:'
+    name_of_game = gets.chomp.to_s
+    puts 'Is it multiplayer yes [y] or [n]: '
+    multiplayer = gets.chomp.to_s
+    puts 'How many years last time played:'
+    last_played_at = gets.chomp.to_s
+    puts 'Firstname of author:'
+    first_name = gets.chomp.to_s
+    puts 'Lastname of author:'
+    last_name = gets.chomp.to_s
+    game = Game.new(name_of_game, multiplayer, last_played_at, first_name, last_name)
+    author = Author.new(first_name, last_name)
+    @games.push(game)
+    @authors.push(author)
+    puts 'Game added successfully'
+  end
+
+  def list_all_games
+    if @games.empty?
+      puts 'The list is empty'
+    else
+      @games.each do |game|
+        # rubocop:todo Layout/LineLength
+        puts "'#{game.name_of_game}' by #{game.first_name} #{game.last_name}, Multiplayer: #{game.multiplayer}, Last time played: #{game.last_played_at}"
+        # rubocop:enable Layout/LineLength
+      end
+    end
+  end
+
+  def list_all_authors
+    if @authors.empty?
+      puts 'The list is empty'
+    else
+      @authors.each do |author|
+        puts "Author name: #{author.first_name} #{author.last_name}"
+      end
+    end
+  end
+end
