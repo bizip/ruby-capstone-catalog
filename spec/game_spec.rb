2955d474e2cad3b0809a8e178da8d065e67b70ec
Ghilain <ishimweghilain5050@gmail.com>
2022-08-11 01:53:07 +0200

    Updates games test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require_relative '../game'
 
 describe Game do
   before :each do
-    @game = Game.new('Ishi', 3)
+    @game = Game.new('mortal', 'y', 3, 'gh', 'ish')
   end
   describe '#new' do
     it 'Creates a new game' do
@@ -10,8 +10,8 @@ describe Game do
     end
   end
   describe '#can_be_archived' do
-    it 'It should reurn true if archived is  equals true and last_played_at > 2' do
-      expect(@game.can_be_archived?).to eql true
+    it 'It should reurn false if archived is not equals to true and last_played_at > 2' do
+      expect(@game.can_be_archived?).to eql false
     end
   end
 end
