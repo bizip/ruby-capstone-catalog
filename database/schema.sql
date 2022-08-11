8538e1430d3f052875b0c50227d3d81afa58d0d5
bizip <bizip04@gmail.com>
2022-08-12 00:07:40 +0200

    Add scheema for music album and genre

--- database/schema.sql
+++ database/schema.sql
@@ -22,3 +22,18 @@ CREATE TABLE labels(
   title VARCHAR(100),
   color VARCHAR(50)
 );
+
+CREATE TABLE music_album(
+  ID SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  on_spotify BOOLEAN NOT NULL,
+  archived BOOLEAN NOT NULL,
+  label_ID INT REFERENCES label(ID),
+  author_ID INT REFERENCES author(ID),
+  genre_ID INT REFERENCES genre(ID)
+);
+
+CREATE TABLE genre(
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR(30)
+);
